8ed7ce99763fde9723bb04c510d318e50ee5f1fe
yumip1997 <yumip1997@gmail.com>
2022-03-22 22:14:46 +0900

    Jwt 상수 추가

---
+++ src/main/java/com/example/jwttutorial/jwt/JwtConstants.java
@@ -0,0 +1,7 @@
+package com.example.jwttutorial.jwt;
+
+public class JwtConstants {
+
+    public static final String AUTHORITIES_KEY = "auth";
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+}
