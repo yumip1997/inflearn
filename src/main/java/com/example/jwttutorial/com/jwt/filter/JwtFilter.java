922a534fd1e9d6e3506a817f161ab418c43c4050
YUMI <yumip1997@gmail.com>
2022-04-13 14:32:32 +0900

    필터 수정

--- src/main/java/com/example/jwttutorial/com/jwt/filter/JwtFilter.java
+++ src/main/java/com/example/jwttutorial/com/jwt/filter/JwtFilter.java
@@ -2,8 +2,6 @@ package com.example.jwttutorial.com.jwt.filter;
 
 import com.example.jwttutorial.com.jwt.JwtConstants;
 import com.example.jwttutorial.com.jwt.util.JwtTokenProvider;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
@@ -14,37 +12,27 @@ import java.io.IOException;
 
 public class JwtFilter extends GenericFilter {
 
-    private static final Logger logger = LoggerFactory.getLogger(JwtFilter.class);
-
     private JwtTokenProvider jwtTokenProvider;
 
     public JwtFilter(JwtTokenProvider jwtTokenProvider) {
         this.jwtTokenProvider = jwtTokenProvider;
     }
 
-    //토근의 인증정보를 SecurityContext에 저장함
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-        String token = getTokenFromRequest(httpServletRequest);
-
-        //TODO 확인필요
-        if(!StringUtils.hasText(token)) return;
-
-        String requestURI = httpServletRequest.getRequestURI();
-
-        try{
-            jwtTokenProvider.validate(token);
-            Authentication authentication = jwtTokenProvider.getAuthentication(token);
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-        }catch (Exception e){
-            logger.debug("유효한 JWT 토근이 없습니다.{}, uri : {}", e.getMessage(), requestURI);
-        }
-
+        setAuthenticationByToken((HttpServletRequest) servletRequest);
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
-    //HttpServletRequest의 header에서 jwt token을 가져옴
+    private void setAuthenticationByToken(HttpServletRequest httpServletRequest){
+        String token = getTokenFromRequest(httpServletRequest);
+        if(!StringUtils.hasText(token)) return;
+        jwtTokenProvider.validate(token);
+        //TODO 토근을 재생성하는 건가??
+        Authentication authentication = jwtTokenProvider.getAuthentication(token);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
     private String getTokenFromRequest(HttpServletRequest request) {
         String bearToken = request.getHeader(JwtConstants.AUTHORIZATION_HEADER);
         return StringUtils.hasText(bearToken) && bearToken.startsWith("Bearer ") ? bearToken.substring(7) : "";
