abad71af3f34829205628029acd685f539484976
yumip1997 <yumip1997@gmail.com>
2022-09-04 20:51:06 +0900

    nuxt

---
+++ layouts/TheError.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>에러 페이지 입니다.</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheError"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <header>
+        <h1> {{$route.name}} 페이지 입니다.</h1>
+        <NuxtLink to = "/">홈페이지</NuxtLink>
+        <NuxtLink to ="/AppMain">메인 페이지</NuxtLink>
+        <NuxtLink to ="/AppProduct">상품 페이지</NuxtLink>
+    </header>
+    <Nuxt/>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
