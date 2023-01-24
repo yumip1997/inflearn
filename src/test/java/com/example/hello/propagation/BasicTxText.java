269c3841bc82c609bee5e107fa8fc90b98a64129
yumip1997 <yumip1997@gmail.com>
2023-01-24 19:39:25 +0900

    트랜잭션 전파

---
+++ src/test/java/com/example/hello/propagation/BasicTxText.java
@@ -0,0 +1,179 @@
+package com.example.hello.propagation;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.UnexpectedRollbackException;
+import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
+
+import javax.sql.DataSource;
+
+@Slf4j
+@SpringBootTest
+public class BasicTxText {
+
+    @Autowired
+    PlatformTransactionManager txManager;
+
+    @TestConfiguration
+    static class Config {
+
+        @Bean
+        public PlatformTransactionManager transactionManager(DataSource dataSource){
+            return new DataSourceTransactionManager(dataSource);
+        }
+
+    }
+
+    @Test
+    void commit(){
+        log.info("트랜잭션 시작");
+        TransactionStatus transaction = txManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("트랜잭션 커밋 시작");
+        txManager.commit(transaction);
+        log.info("트랜잭션 커밋 완료");
+    }
+
+    @Test
+    void rollback(){
+        log.info("트랜잭션 시작");
+        TransactionStatus transaction = txManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("트랜잭션 롤백 시작");
+        txManager.rollback(transaction);
+        log.info("트랜잭션 롤백 완료");
+    }
+
+    @Test
+    void double_commit(){
+        log.info("트랜잭션1 시작");
+        TransactionStatus transaction1 = txManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("트랜잭션1 커밋 시작");
+        txManager.commit(transaction1);
+        log.info("트랜잭션1 커밋 완료");
+
+        log.info("트랜잭션2 시작");
+        TransactionStatus transaction2 = txManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("트랜잭션2 커밋 시작");
+        txManager.commit(transaction2);
+        log.info("트랜잭션2 커밋 완료");
+    }
+
+    @Test
+    void double_commit_rollback(){
+        log.info("트랜잭션1 시작");
+        TransactionStatus transaction1 = txManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("트랜잭션1 커밋 시작");
+        txManager.commit(transaction1);
+        log.info("트랜잭션1 커밋 완료");
+
+        log.info("트랜잭션2 시작");
+        TransactionStatus transaction2 = txManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("트랜잭션2 롤백 시작");
+        txManager.rollback(transaction2);
+        log.info("트랜잭션2 롤백 완료")  ;
+    }
+
+
+    @Test
+    void outer_rollback(){
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("내부 트랜잭션 커밋");
+        txManager.commit(inner);
+
+        log.info("외부 트랜잭션 롤백");
+        txManager.rollback(outer);
+    }
+
+    @Test
+    void inner_rollback(){
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("내부 트랜잭션 롤백"); //rollback only 마킹 - rollback 되어야한다는 표시
+        txManager.rollback(inner);
+        // UnexpectedRollbackException 예외 발생 : Transaction rolled back because it has been marked as rollback-only
+        // 트랜잭션 매니저는 신규 트랜잭션 여부에 따라 다르게 동작 -> inner 신규 트랜잭션이 아니기 때문에 실제 롤백이 일어나지 않음
+        // 대신 트랜잭션 동기화 매니저에 rollbackOnly = true 라는 표시를 해둔다.
+
+        log.info("외부 트랜잭션 커밋");
+        //commit 전, 트랜잭션 동기화 매니저의 rollbackOnly = true 인지 확인
+        //true 라면 commit 되지 않고 rollback 된다.
+        Assertions.assertThatThrownBy(() -> txManager.commit(outer)).isInstanceOf(UnexpectedRollbackException.class);
+    }
+
+    @Test
+    void outer_commit(){
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("트랜잭션 커밋");
+        txManager.commit(inner);
+
+        inner_commit();
+    }
+
+    void inner_commit(){
+        log.info("호출된 트랜잭션 커밋");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+        txManager.commit(inner);
+    }
+
+    @Test
+    void test(){
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("내부 트랜잭션 롤백"); //rollback only 마킹 - rollback 되어야한다는 표시
+        txManager.rollback(inner);
+        // UnexpectedRollbackException 예외 발생 : Transaction rolled back because it has been marked as rollback-only
+        // 트랜잭션 매니저는 신규 트랜잭션 여부에 따라 다르게 동작 -> inner 신규 트랜잭션이 아니기 때문에 실제 롤백이 일어나지 않음
+        // 대신 트랜잭션 동기화 매니저에 rollbackOnly = true 라는 표시를 해둔다.
+
+        log.info("외부 트랜잭션 커밋");
+        //commit 전, 트랜잭션 동기화 매니저의 rollbackOnly = true 인지 확인
+        //true 라면 commit 되지 않고 rollback 된다.
+        Assertions.assertThatThrownBy(() -> txManager.commit(outer)).isInstanceOf(UnexpectedRollbackException.class);
+    }
+
+    @Test
+    void inner_rollback_requires_new(){
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.isNewTransaction() = {}", outer.isNewTransaction());
+
+        log.info("내부 트랜잭션 시작");
+        DefaultTransactionAttribute defaultTransactionAttribute = new DefaultTransactionAttribute();
+        defaultTransactionAttribute.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+        TransactionStatus inner = txManager.getTransaction(defaultTransactionAttribute);    // Suspending current transaction, creating new transaction -> 커넥션 획득, 수동 커밋
+        log.info("inner.isNewTransaction() = {}", inner.isNewTransaction());
+
+        log.info("내부 트랜잭션 롤백");
+        txManager.rollback(inner);  //커넥션 반환
+
+        log.info("외부 트랜잭션 커밋");
+        txManager.commit(outer);
+    }
+
+}
+
+
