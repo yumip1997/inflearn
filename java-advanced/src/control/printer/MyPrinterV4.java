fd77b6c8a83631862d9db09f0025ec315ca3b124
yumip1997 <yumip1997@gmail.com>
2024-12-03 22:24:47 +0900

    yield 정리

---
+++ java-advanced/src/control/printer/MyPrinterV4.java
@@ -0,0 +1,62 @@
+package control.printer;
+
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import static util.MyLogger.log;
+
+public class MyPrinterV4 {
+
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+        Thread printerThread = new Thread(printer, "printer");
+        printerThread.start();
+
+        final Scanner userInput = new Scanner(System.in);
+        while (true){
+            log("enter string [exit (q)] :");
+            final String input = userInput.nextLine();
+
+            // printerThread run 메서드가 바로 종료되지 않는다! 최대 3초정도의 지연이 있다!
+            if(input.equals("q")){
+                printerThread.interrupt();
+                break;
+            }
+
+            printer.addJob(input);
+        }
+    }
+
+    static class Printer implements Runnable {
+        Queue<String> jobQueue = new ConcurrentLinkedQueue<>();
+
+        @Override
+        public void run() {
+            while (!Thread.interrupted()){  // 1초에 수억번 반복될 수도 있다!
+                if(jobQueue.isEmpty()){
+                    Thread.yield(); // 다른 쓰레드에 작업을 양보한다 양보하지 않을 떄보다 cpu를 효율적으로 쓸 수 있음
+                    continue;
+                }
+
+                try{
+                    String job = jobQueue.poll();
+                    log("print start : " + job + ", waiting document : " + jobQueue);
+                    Thread.sleep(3000);
+                }catch (InterruptedException e){
+                    log("thread interrupted state1 : " + Thread.currentThread().isInterrupted());
+                    log("interrupted!");
+                    break;
+                }
+            }
+
+            log("thread interrupted state2 : " + Thread.currentThread().isInterrupted());
+            log("print complete");
+        }
+
+        public void addJob(String input){
+            jobQueue.offer(input);
+        }
+    }
+
+}
