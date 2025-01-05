22be31a223aa4f900f788d5871b61e1782bb0322
yumip1997 <yumip1997@gmail.com>
2025-01-05 23:13:22 +0900

    메모리 가시성 문제

---
+++ java-advanced/src/control/volatile1/VolatileFlagMain.java
@@ -0,0 +1,38 @@
+package control.volatile1;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class VolatileFlagMain {
+
+    public static void main(String[] args) {
+        MyTask task = new MyTask();
+        Thread thread = new Thread(task, "work");
+        log("runFlag = " + task.runFlag);
+        thread.start();
+
+        sleep(1000);
+        log("runFlag를 false로 변경 시도");
+        task.runFlag = false;
+        log("runFlag = " + task.runFlag);
+        log("main 종료");
+
+        // task는 while문을 탈출하지 못한다!!
+    }
+
+    static class MyTask implements Runnable {
+
+        boolean runFlag = true;
+
+        @Override
+        public void run() {
+            log("task 시작");
+
+            while (runFlag){
+
+            }
+
+            log("task 종료");
+        }
+    }
+}
