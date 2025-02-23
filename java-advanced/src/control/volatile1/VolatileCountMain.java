623f49bddadcb71fffbea5cd588e12d6820a8064
박유미 <yumip1008@plateer.com>
2025-02-23 13:41:56 +0900

    volatile 정리

---
+++ java-advanced/src/control/volatile1/VolatileCountMain.java
@@ -0,0 +1,37 @@
+package control.volatile1;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class VolatileCountMain {
+
+    public static void main(String[] args) {
+        MyTask task = new MyTask();
+        Thread t = new Thread(task, "work");
+        t.start();
+
+        sleep(1000);
+        task.flag = false;
+
+        log("flag = " + task.flag + ", count = " + task.count + " in main");
+    }
+
+    static class MyTask implements Runnable {
+
+        volatile boolean flag = true;
+        long count;
+
+        @Override
+        public void run() {
+            while (flag){
+                count++;
+
+                if(count % 100_000_000 == 0){
+                    log("flag = " + flag + ", count = " + count + " in while()");
+                }
+            }
+
+            log("flag = " + flag + ", count = " + count + " 종료");
+        }
+    }
+}
