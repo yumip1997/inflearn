168a0bce34a136b3e30fa8eeb603604234698d08
yumip1997 <yumip1997@gmail.com>
2025-02-28 21:59:40 +0900

    synchronized 키워드 사용

---
+++ java-advanced/src/sync/BankAccountV2.java
@@ -0,0 +1,41 @@
+package sync;
+
+import static util.MyLogger.log;
+
+public class BankAccountV2 implements BankAccount {
+
+    private int balance;
+
+    public BankAccountV2(int initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    @Override
+    public synchronized boolean withdraw(int amount) {
+        log("거래 시작 : " + getClass().getSimpleName());
+
+        log("[검증 시작] 출금액: " + amount + ", 잔액: " + balance);
+
+        // 잔고가 출금액 보다 적으면 false
+        if(balance < amount) {
+            log("[검증 실패]");
+            return false;
+        }
+
+        // 잔고가 출금액보다 많으면 진행
+        log("[검증 완료] 출금액: " + amount + ", 잔액: " + balance);
+        //sleep(1000);
+
+        balance -= amount;
+
+        log("[출금 완료] 출금액: " + amount + ", 잔액: " + balance);
+
+        log("거래 종료");
+        return true;
+    }
+
+    @Override
+    public int getBalance() {
+        return balance;
+    }
+}
